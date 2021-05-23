1e8c1a6204d01f764f8a078255a413ca38f3c13c
Tusar kanta mallick <tusarkanta.mallick@mindtraits.in>
2021-05-23 20:51:40 +0530

    testcase fixed

--- src/app/product/product.component.spec.ts
+++ src/app/product/product.component.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductComponent } from './product.component';
@@ -8,7 +9,8 @@ describe('ProductComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProductComponent ]
+      declarations: [ ProductComponent ],
+      imports: [HttpClientModule],
     })
     .compileComponents();
   }));
